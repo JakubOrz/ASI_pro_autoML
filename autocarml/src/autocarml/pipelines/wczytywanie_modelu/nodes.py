dfa0f27ba5cb13aa955f5ecc789985930143dad3
Błażej Bartkiewicz <s16752@pjwstk.edu.pl>
2023-01-29 23:03:59 +0100

    fixed model pipeline

--- autocarml/src/autocarml/pipelines/wczytywanie_modelu/nodes.py
+++ autocarml/src/autocarml/pipelines/wczytywanie_modelu/nodes.py
@@ -5,16 +5,16 @@ generated using Kedro 0.18.4
 import pickle
 from typing import Any
 
-import pandas
 from pycaret.regression import load_model, predict_model
 import pandas as pd
 
 
-def predict(model, data: pd.DataFrame) -> Any:
+def predict(model_path: str, data: pd.DataFrame) -> float:
     predict_data = [[2014, 'BMW', '3 Series', '328i SULEV', 'Sedan', 'automatic', 4.5, 1331.0, 'gray', 'black', 31900]]
     data_frame = pd.DataFrame(predict_data, columns=
     ['year', 'make', 'model', 'trim', 'body', 'transmission', 'condition', 'odometer', 'color', 'interior', 'mmr']
                               )
-    result = predict_model(model, data=data)
-    # print(result)
+    model = load_model(model_path)
+    result_df = predict_model(model, data=data)
+    result = result_df.iloc[0]['Label']
     return result
